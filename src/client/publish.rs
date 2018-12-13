b72afb57015ab593efb695c66c03beecfa11a3af
Arnav Singh <arsing@microsoft.com>
2018-12-13 12:54:44 -0800

    Ignore duplicate PUBLISH packets.

--- src/client/publish.rs
+++ src/client/publish.rs
@@ -2,6 +2,7 @@
 pub(super) struct State {
 	packet_identifiers: super::PacketIdentifiers,
 
+	previously_seen_publish_packets: PreviouslySeenPackets,
 	at_least_once_waiting_to_be_acked:
 		std::collections::BTreeMap<crate::proto::PacketIdentifier, (futures::sync::oneshot::Sender<()>, crate::proto::Packet)>,
 }
@@ -31,26 +32,53 @@ impl State {
 			Some(crate::proto::Packet::PubComp { .. }) => unimplemented!(),
 
 			Some(crate::proto::Packet::Publish { packet_identifier_dup_qos, retain, topic_name, payload }) => {
-				let qos = match packet_identifier_dup_qos {
-					crate::proto::PacketIdentifierDupQoS::AtMostOnce => crate::proto::QoS::AtMostOnce,
-					crate::proto::PacketIdentifierDupQoS::AtLeastOnce(_, _) => crate::proto::QoS::AtLeastOnce,
-					crate::proto::PacketIdentifierDupQoS::ExactlyOnce(_, _) => crate::proto::QoS::ExactlyOnce,
+				// If the server sends a duplicate PUBLISH packet and its identifier is in the previously-seen set,
+				// then we assume the server did not receive the ack we sent the last time we received this packet.
+				// So we ack it again but *don't* forward it to the client.
+				let previously_seen_packet_identifier = match packet_identifier_dup_qos {
+					crate::proto::PacketIdentifierDupQoS::AtMostOnce =>
+						None,
+
+					crate::proto::PacketIdentifierDupQoS::AtLeastOnce(packet_identifier, dup) =>
+						if dup && self.previously_seen_publish_packets.previously_seen(packet_identifier) {
+							log::debug!("ignoring duplicate PUBLISH packet {}", packet_identifier);
+							Some(packet_identifier)
+						}
+						else {
+							None
+						},
+
+					crate::proto::PacketIdentifierDupQoS::ExactlyOnce(_, _) =>
+						unimplemented!(),
 				};
 
-				publications_received.push(crate::Publication {
-					topic_name,
-					qos,
-					retain,
-					payload,
-				});
+				if let Some(packet_identifier) = previously_seen_packet_identifier {
+					packets_waiting_to_be_sent.push(crate::proto::Packet::PubAck {
+						packet_identifier,
+					});
+				}
+				else {
+					let qos = match packet_identifier_dup_qos {
+						crate::proto::PacketIdentifierDupQoS::AtMostOnce => crate::proto::QoS::AtMostOnce,
+						crate::proto::PacketIdentifierDupQoS::AtLeastOnce(_, _) => crate::proto::QoS::AtLeastOnce,
+						crate::proto::PacketIdentifierDupQoS::ExactlyOnce(_, _) => crate::proto::QoS::ExactlyOnce,
+					};
 
-				match packet_identifier_dup_qos {
-					crate::proto::PacketIdentifierDupQoS::AtMostOnce => (),
-					crate::proto::PacketIdentifierDupQoS::AtLeastOnce(packet_identifier, _) =>
-						packets_waiting_to_be_sent.push(crate::proto::Packet::PubAck {
-							packet_identifier,
-						}),
-					crate::proto::PacketIdentifierDupQoS::ExactlyOnce(_, _) => unimplemented!(),
+					publications_received.push(crate::Publication {
+						topic_name,
+						qos,
+						retain,
+						payload,
+					});
+
+					match packet_identifier_dup_qos {
+						crate::proto::PacketIdentifierDupQoS::AtMostOnce => (),
+						crate::proto::PacketIdentifierDupQoS::AtLeastOnce(packet_identifier, _) =>
+							packets_waiting_to_be_sent.push(crate::proto::Packet::PubAck {
+								packet_identifier,
+							}),
+						crate::proto::PacketIdentifierDupQoS::ExactlyOnce(_, _) => unimplemented!(),
+					}
 				}
 			},
 
@@ -115,7 +143,42 @@ impl Default for State {
 		State {
 			packet_identifiers: Default::default(),
 
+			previously_seen_publish_packets: Default::default(),
 			at_least_once_waiting_to_be_acked: Default::default(),
 		}
+	}
+}
+
+struct PreviouslySeenPackets([u8; PreviouslySeenPackets::SIZE]);
+
+impl PreviouslySeenPackets {
+	/// Size of a bitset for every packet identifier
+	///
+	/// Packet identifiers are u16's, and there are 8 bits to a byte, so the size
+	/// = number of u16's / 8
+	/// = pow(2, number of bits in a u16) / 8
+	/// = pow(2, 16) / 8
+	const SIZE: usize = 8192;
+
+	/// Returns `true` if this packet was seen before, `false` if it was not
+	fn previously_seen(&mut self, packet_identifier: crate::proto::PacketIdentifier) -> bool {
+		let packet_identifier = packet_identifier.get();
+		let (block, offset) = ((packet_identifier / 8) as usize, (packet_identifier % 8) as usize);
+		let mask = 1 << offset;
+		let is_set = (self.0[block] & mask) != 0;
+		self.0[block] |= mask;
+		is_set
+	}
+}
+
+impl std::fmt::Debug for PreviouslySeenPackets {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		f.write_str("PreviouslySeenPackets")
+	}
+}
+
+impl Default for PreviouslySeenPackets {
+	fn default() -> Self {
+		PreviouslySeenPackets([0; PreviouslySeenPackets::SIZE])
 	}
 }
